c513ea5efd38b82f9f583543805166f6f391a9ea
Randy Ehret <randyehret@gmail.com>
2017-04-30 00:08:18 -0500

    Allow celestials to move "up" a physics grid

--- app/classes/celestial.ts
+++ app/classes/celestial.ts
@@ -24,6 +24,28 @@ class celestial {
             let d = this.getDistance(this.parent);
             let g = physics.getGravityAcceleration(this.parent.mass, d);
 
+            // check if object should move "up" one physics grid
+            if (this.parent.parent !== null) {
+                let grandparent = this.parent.parent;
+                let grandparentDistance = this.getDistance(grandparent);
+                let grandparentG = physics.getGravityAcceleration(grandparent.mass, grandparentDistance);
+
+                // decide to move up a grid if ratio G:distance is greater for grandparent
+                // basically, all descendants of the star will see the star as the highest G in the system
+                // so we use this ratio to find the effect of gravity on the celestial, biased towards
+                // the current parent
+                let parentGravityToDistanceRatio = g / d;
+                let grandparentGravityToDistanceRatio = grandparentG / grandparentDistance;
+
+                if (grandparentGravityToDistanceRatio > parentGravityToDistanceRatio) {
+                    this.position = this.position.add(this.parent.position);
+                    this.velocity = this.velocity.add(this.parent.velocity);
+                    this.parent = this.parent.parent;
+                    g = grandparentG;
+                    d = grandparentDistance;
+                }
+            }
+
             let start = new THREE.Vector3(this.getWorldCoords().x, this.getWorldCoords().y, 0);
             let end = new THREE.Vector3(this.parent.getWorldCoords().x, this.parent.getWorldCoords().y, 0);
             let gVector = new THREE.Line3(start, end).delta().normalize();
@@ -31,8 +53,7 @@ class celestial {
             this.velocity.x += gVector.x * g;
             this.velocity.y += gVector.y * g;
 
-            this.position.x += this.velocity.x;
-            this.position.y += this.velocity.y;
+            this.position = this.position.add(this.velocity);
         }
     }
 
--- app/classes/pair.ts
+++ app/classes/pair.ts
@@ -6,4 +6,15 @@ class pair {
         this.x = 0;
         this.y = 0;
     }
+
+    static add(p1: pair, p2: pair) {
+        let sum = new pair();
+        sum.x = p1.x + p2.x;
+        sum.y = p1.y + p2.y;
+        return sum;
+    }
+
+    add(p: pair) {
+        return pair.add(this, p);
+    }
 }
