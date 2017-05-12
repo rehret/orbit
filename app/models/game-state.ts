801062668c9b6e62e947367ee00ee21b3436d637
Randy Ehret <randyehret@gmail.com>
2017-05-11 21:12:28 -0500

    Add a simple HUD

--- app/models/game-state.ts
+++ app/models/game-state.ts
@@ -17,6 +17,9 @@ class GameState {
     Update() {
         this.Celestials.forEach(c => c.update());
         this.Player = this.Player.Update();
+        let velocity = Math.sqrt(Math.pow(this.Player.PlayerObject.velocity.x, 2) + Math.pow(this.Player.PlayerObject.velocity.y, 2));
+        document.getElementById("velocity").innerText = `${velocity.toFixed(2)} m/s`;
+        document.getElementById("velocity-in-c").innerText = `${(velocity / physics.C).toFixed(7)}c`;
     }
 
     CameraZoom(zoomAmount) {
