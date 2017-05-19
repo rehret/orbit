e765cbb203751fc572f5e0b227ee44aa7c0b473d
Randy Ehret <rehret@frontlineed.com>
2017-05-19 08:52:40 -0500

    Add dev webpack config

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const webpack = require('webpack');
+const path = require('path');
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+/*
+ * We've enabled ExtractTextPlugin for you. This allows your app to
+ * use css modules that will be moved into a separate CSS file instead of inside
+ * one of your module entries!
+ *
+ * https://github.com/webpack-contrib/extract-text-webpack-plugin
+ *
+ */
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: './app/orbits.ts',
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader',
+				exclude: '/node_modules/'
+			},
+			{
+				test: /\.(scss|css)$/,
+				use: ExtractTextPlugin.extract({
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: true
+							}
+						}
+					],
+					fallback: 'style-loader'
+				})
+			},
+			{
+				enforce: "pre",
+				test: /\.js$/,
+				loader: "source-map-loader"
+			},
+			{
+				enforce: "pre",
+				test: /\.tsx?$/,
+				use: "source-map-loader"
+			}
+		]
+	},
+
+	resolve: {
+		extensions: [".tsx", ".ts", "js"]
+	},
+
+	devtool: "inline-source-map",
+
+	plugins: [new ExtractTextPlugin('style.css')]
+};
